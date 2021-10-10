889376162b415e648ee32b34729da2f3434737c8
PigalAnastasiya <pigal.anastasiya@gmail.com>
2021-10-10 16:45:32 +0300

    feat: add solution of homework4

---
+++ TeachMeSkills.Pigal.Homework4/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TeachMeSkills.Pigal.Homework4
+{
+    internal class Todo
+    {
+        private readonly DateTime _dateTime = DateTime.Now;
+
+        public DateTime TaskTime { get; set; }
+        public string Description { get; set; }
+        public DateTime TaskDuration { get; set; }
+
+        public void GetInfo()
+        {
+            Console.BufferWidth = 150;
+            Console.WriteLine($"Task: {Description}\tTask duration: {TaskTime} => {TaskDuration}\tTask create: {_dateTime}");
+        }
+    }
+}
