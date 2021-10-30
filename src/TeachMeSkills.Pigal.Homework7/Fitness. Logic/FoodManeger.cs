fceacde84347d1b8a84901c6c4b9b72cf127636a
PigalAnastasiya <pigal.anastasiya@gmail.com>
2021-10-30 16:34:36 +0300

    feat:add solution of homework7

---
+++ src/TeachMeSkills.Pigal.Homework7/Fitness. Logic/FoodManeger.cs
@@ -0,0 +1,191 @@
+﻿using Fitness.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fitness.Library.Logic
+{
+    public class FootManeger
+    {
+        public List<Food> Foods = new List<Food>();
+        public List<double> Calories = new List<double>();
+        public List<double> Proteins = new List<double>();
+        public List<double> Fats = new List<double>();
+        public List<double> Cabohydrates = new List<double>();
+        private double sumCalories = 0;
+        private double sumProteins = 0;
+        private double sumFats = 0;
+        private double sumCabohydrates = 0;
+
+        public bool IsNewFood { get; set; } = false;
+        public Food CurrentFood { get; set; }
+
+        public void CheckingFood(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Название блюда не может быть пустым", nameof(name));
+            }
+            CurrentFood = Foods.SingleOrDefault(c => c.Name == name);
+            if (CurrentFood == null)
+            {
+                CurrentFood = new Food(name);
+
+                CreateNewFood(IsNewFood = true);
+            }
+        }
+
+        /// <summary>
+        /// Создание новой еды
+        /// </summary>
+        /// <param name="IsNewFood"></param>
+        public void CreateNewFood(bool IsNewFood)
+        {
+            if (IsNewFood)
+            {
+                double proteins = ParseDouble("белки");
+                double fats = ParseDouble("жиры");
+                double cabohydrates = ParseDouble("углеводы");
+                double calories = ParseDouble("калории");
+
+                SetNewFoodData(proteins, fats, cabohydrates, calories);
+            }
+
+            Console.WriteLine($"Добавлено новое блюдо: {CurrentFood} - в 100 гр калорий {CurrentFood.Calories}");
+        }
+
+        /// <summary>
+        /// Добавление новой еды в соответствующие списки
+        /// </summary>
+        /// <param name="proteins">белки</param>
+        /// <param name="fats">жиры</param>
+        /// <param name="cabohydrates">углеводы</param>
+        /// <param name="calories">калории</param>
+        public void SetNewFoodData(double proteins, double fats, double cabohydrates, double calories)
+        {
+            //TODO проверка
+            CurrentFood.Proteins = proteins;
+            CurrentFood.Fats = fats;
+            CurrentFood.Cabohydrates = cabohydrates;
+            CurrentFood.Calories = calories;
+            Calories.Add(calories);
+            Proteins.Add(proteins);
+            Fats.Add(fats);
+            Cabohydrates.Add(cabohydrates);
+            Foods.Add(CurrentFood);
+        }
+
+        /// <summary>
+        /// Превод в double
+        /// </summary>
+        /// <param name="name">значение</param>
+        /// <returns></returns>
+        private static double ParseDouble(string name)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Введите {name}");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат ввода ", nameof(name));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Посчет суммы калорий
+        /// </summary>
+        /// <param name="Calories">калории</param>
+        /// <returns></returns>
+        public double SumCalories(List<double> Calories)
+        {
+            foreach (double i in Calories)
+            {
+                sumCalories += i;
+            }
+            return sumCalories;
+        }
+
+        /// <summary>
+        /// Подсчет количества белков
+        /// </summary>
+        /// <param name="Proteins">белки</param>
+        /// <returns></returns>
+        public double SumProteins(List<double> Proteins)
+        {
+            foreach (double i in Proteins)
+            {
+                sumProteins += i;
+            }
+            return sumProteins;
+        }
+
+        /// <summary>
+        /// Подсчет количества жиры
+        /// </summary>
+        /// <param name="Fats">жиры</param>
+        /// <returns></returns>
+        public double SumFats(List<double> Fats)
+        {
+            foreach (double i in Fats)
+            {
+                sumFats += i;
+            }
+            return sumFats;
+        }
+
+        /// <summary>
+        /// Подсчет количества углеводов
+        /// </summary>
+        /// <param name="Cabohydrates">углеводы</param>
+        /// <returns></returns>
+        public double SumCabohydrates(List<double> Cabohydrates)
+        {
+            foreach (double i in Cabohydrates)
+            {
+                sumCabohydrates += i;
+            }
+            return sumCabohydrates;
+        }
+
+        /// <summary>
+        /// Вывод количество углеводов
+        /// </summary>
+        /// <returns></returns>
+        public double ShowSumCabohydrates()
+        {
+            return SumCabohydrates(Cabohydrates);
+        }
+
+        /// <summary>
+        /// Вывод количесво железа
+        /// </summary>
+        /// <returns></returns>
+        public double ShowSumFats()
+        {
+            return SumFats(Fats);
+        }
+
+        /// <summary>
+        /// Вывод количества белков
+        /// </summary>
+        /// <returns></returns>
+        public double ShowSumProteins()
+        {
+            return SumProteins(Proteins);
+        }
+
+        /// <summary>
+        /// Вывод количество калорий
+        /// </summary>
+        /// <returns></returns>
+        public double ShowSumCalories()
+        {
+            return SumCalories(Calories);
+        }
+    }
+}
---
+++ src/TeachMeSkills.Pigal.Homework7/Fitness. Logic/UserManager.cs
@@ -0,0 +1,131 @@
+﻿using Fitness.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fitness.Library.Logic
+{
+    public class UserManager
+    {
+        private List<Account> client = new List<Account>();
+        protected List<string> users = new List<string>();
+        public Account CurrentUser { get; set; }
+        public bool IsNewUser { get; set; } = false;
+
+        /// <summary>
+        /// Проверка логина.
+        /// </summary>
+        /// <param name="userName">логин</param>
+        public void CheckingLogin(string userName)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым", nameof(userName));
+            }
+            CurrentUser = client.SingleOrDefault(c => c.Login == userName);
+            if (CurrentUser == null)
+            {
+                users.Add(userName);
+                CurrentUser = new Account(userName);
+
+                CreateAccount(IsNewUser = true);
+            }
+        }
+
+        /// <summary>
+        /// Создание аккаунта.
+        /// </summary>
+        public void CreateAccount(bool IsNewUser)
+        {
+            if (IsNewUser)
+            {
+                Console.WriteLine("Введите пол");
+                var gender = Console.ReadLine();
+
+                double weight = ParseDouble("вес");
+                double height = ParseDouble("рост");
+                DateTime birthDate = ParseDateTime();
+
+                SetNewUserData(gender, birthDate, weight, height);
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Зарегистрированн новый пользователь: {CurrentUser}");
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Приведение из string в double
+        /// </summary>
+        /// <param name="name">переменная типа string</param>
+        /// <returns></returns>
+        private static double ParseDouble(string name)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Введите {name}");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат ввода ", nameof(name));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Приведение из string в DateTime
+        /// </summary>
+        /// <returns></returns>
+        private static DateTime ParseDateTime()
+        {
+            DateTime birthDate;
+            while (true)
+            {
+                Console.WriteLine("Введите дату рождения");
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат даты рождения");
+                }
+            }
+
+            return birthDate;
+        }
+
+        /// <summary>
+        /// Запись в список нового пользователя
+        /// </summary>
+        /// <param name="genderName">пол</param>
+        /// <param name="birthday">день рождения</param>
+        /// <param name="weight">вес</param>
+        /// <param name="height">рост</param>
+        public void SetNewUserData(string genderName, DateTime birthday,
+            double weight = 1, double height = 1)
+        {
+            //TODO проверка
+            CurrentUser.Gender = new Gender(genderName);
+            CurrentUser.BirthDate = birthday;
+            CurrentUser.Weight = weight;
+            CurrentUser.Height = height;
+            client.Add(CurrentUser);
+        }
+
+        public void ShowUser(string name)
+        {
+            foreach (Account p in client)
+            {
+                if (p.Login == name)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(p);
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
